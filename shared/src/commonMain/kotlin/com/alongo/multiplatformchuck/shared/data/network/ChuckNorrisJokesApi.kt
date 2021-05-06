cf7dc9869ed1019109c8f3bd26d874c0556eff70
vlavet <pdastore1@gmail.com>
2021-05-07 02:00:07 +0300

    Rename GetRandomJokesResponse to Joke

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/network/ChuckNorrisJokesApi.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/network/ChuckNorrisJokesApi.kt
@@ -1,13 +1,13 @@
 package com.alongo.multiplatformchuck.shared.data.network
 
 import com.alongo.multiplatformchuck.shared.data.network.base.BaseChuckNorrisApi
-import com.alongo.multiplatformchuck.shared.domain.entity.httpentities.response.GetRandomJokesResponse
+import com.alongo.multiplatformchuck.shared.domain.entity.httpentities.jokes.Joke
 
 interface ChuckNorrisJokesApi : BaseChuckNorrisApi {
     override val secondaryUrl: String
         get() = "/jokes"
 
-    suspend fun getRandomJoke(): GetRandomJokesResponse
+    suspend fun getRandomJoke(): Joke
 
 }
 
