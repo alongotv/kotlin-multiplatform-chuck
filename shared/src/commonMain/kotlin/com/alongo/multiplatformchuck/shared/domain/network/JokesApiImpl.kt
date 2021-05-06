c8b8578cbc280073eba1e66bba996ec09e86ecde
vlavet <pdastore1@gmail.com>
2021-05-07 01:59:38 +0300

    Add JokesApiImpl

---
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/domain/network/JokesApiImpl.kt
@@ -0,0 +1,13 @@
+package com.alongo.multiplatformchuck.shared.domain.network
+
+import com.alongo.multiplatformchuck.shared.data.network.ChuckNorrisJokesApi
+import com.alongo.multiplatformchuck.shared.domain.entity.httpentities.jokes.Joke
+import io.ktor.client.*
+import io.ktor.client.request.*
+
+class JokesApiImpl(private val httpClient: HttpClient) : ChuckNorrisJokesApi {
+
+    override suspend fun getRandomJoke(): Joke {
+        return httpClient.get(endpoint + "/random")
+    }
+}
