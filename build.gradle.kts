9bd60260fb7e7a4960295a96bfe6728fb184a717
vlavet <pdastore1@gmail.com>
2021-05-07 02:01:31 +0300

    Update buildscript dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+        classpath("com.android.tools.build:gradle:4.2.0")
     }
 }
 group = "com.alongo.multiplatformchuck"
