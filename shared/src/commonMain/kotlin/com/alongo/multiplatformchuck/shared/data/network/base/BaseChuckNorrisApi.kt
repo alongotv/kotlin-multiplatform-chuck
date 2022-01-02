69e538938c2004970e1e14314ba1880c0a271f08
vlavet <pdastore1@gmail.com>
2022-01-03 01:23:47 +0300

    Refactor ChuckNorrisApi

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/network/base/BaseChuckNorrisApi.kt
+++
@@ -1,7 +0,0 @@
-package com.alongo.multiplatformchuck.shared.data.network.base
-
-interface BaseChuckNorrisApi : BaseApi {
-    override val baseUrl: String
-        get() = "https://api.chucknorris.io"
-
-}
