80e33fd0f39929f6c7d385447526d6f822097ed0
vlavet <pdastore1@gmail.com>
2022-01-03 01:16:34 +0300

    Update di implementation

--- androidApp/src/main/java/com/alongo/multiplatformchuck/androidApp/presentation/base/BaseActivity.kt
+++ androidApp/src/main/java/com/alongo/multiplatformchuck/androidApp/presentation/base/BaseActivity.kt
@@ -1,10 +1,12 @@
 package com.alongo.multiplatformchuck.androidApp.presentation.base
 
 import androidx.appcompat.app.AppCompatActivity
+import com.alongo.multiplatformchuck.shared.di.Injector
 import org.kodein.di.DIAware
-import org.kodein.di.android.closestDI
 
 open class BaseActivity : AppCompatActivity(), DIAware {
 
-    override val di by closestDI()
+    override val di by lazy {
+        Injector.kodeinContainer
+    }
 }
