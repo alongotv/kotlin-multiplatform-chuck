d46ff892e6a0ca5b9340cf17467391f301a50fb4
vlavet <pdastore1@gmail.com>
2022-01-03 01:18:29 +0300

    Remove Platform.kt; Remove Greeting.kt

--- shared/src/androidMain/kotlin/com/alongo/multiplatformchuck/shared/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.alongo.multiplatformchuck.shared
-
-actual class Platform actual constructor() {
-    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.alongo.multiplatformchuck.shared
-
-expect class Platform() {
-    val platform: String
-}
