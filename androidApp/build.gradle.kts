a08d683709a5765b72d0982c6f9c7ef75bd537c0
vlavet <pdastore1@gmail.com>
2022-01-03 01:14:56 +0300

    Update gradle build files for android app

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,26 +9,25 @@ version = "1.0-SNAPSHOT"
 repositories {
     gradlePluginPortal()
     google()
-    jcenter()
     mavenCentral()
 }
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
     implementation("org.kodein.di:kodein-di-framework-android-x:7.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.6.10")
     implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.4.6")
 
 }
 android {
-    compileSdkVersion(30)
+    compileSdk = 31
     defaultConfig {
         applicationId = "com.alongo.multiplatformchuck.androidApp"
-        minSdkVersion(24)
-        targetSdkVersion(30)
+        minSdk = 24
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
     }
@@ -45,6 +44,6 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-       viewBinding = true
+        viewBinding = true
     }
 }
