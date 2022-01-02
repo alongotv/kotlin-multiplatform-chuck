03f018c5bd73f299e1b1a7f4098c83f6177627d9
vlavet <pdastore1@gmail.com>
2022-01-03 01:22:21 +0300

    Refactor BaseApi

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/network/base/BaseApi.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/network/base/BaseApi.kt
@@ -1,10 +1,26 @@
 package com.alongo.multiplatformchuck.shared.data.network.base
 
+import com.alongo.multiplatformchuck.shared.di.Injector
+import io.ktor.client.HttpClient
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.http.Url
+import org.kodein.di.instance
 
-interface BaseApi {
-    val baseUrl: String
-    val secondaryUrl: String
+abstract class BaseApi {
 
-    val endpoint: String
-        get() = baseUrl + secondaryUrl
+    abstract val baseUrl: String
+    val httpClient: HttpClient by Injector.kodeinContainer.instance()
+
+    suspend inline fun <reified T : Any> get(
+        endpoint: String,
+        block: HttpRequestBuilder.() -> Unit = {}
+    ): T =
+        httpClient.get(url = Url(baseUrl + endpoint), block = block)
+
+    suspend inline fun <reified T : Any> post(
+        endpoint: String,
+        block: HttpRequestBuilder.() -> Unit = {}
+    ): T = httpClient.post(url = Url(baseUrl + endpoint), block = block)
 }
