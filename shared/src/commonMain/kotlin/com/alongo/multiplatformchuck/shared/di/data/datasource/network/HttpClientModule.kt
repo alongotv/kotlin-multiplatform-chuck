30a2fb76c8a63c7b75792413e912e4dd189d0def
vlavet <pdastore1@gmail.com>
2022-01-07 02:55:17 +0300

    Add Ktor logging

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/di/data/datasource/network/HttpClientModule.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/di/data/datasource/network/HttpClientModule.kt
@@ -1,9 +1,14 @@
 package com.alongo.multiplatformchuck.shared.di.data.datasource.network
 
 import com.alongo.multiplatformchuck.shared.common.KtorClientEngine
-import io.ktor.client.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
+import io.github.aakira.napier.Napier
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.singleton
@@ -14,6 +19,14 @@ val httpClientModule = DI.Module("HttpClientModule") {
             expectSuccess = false
             install(JsonFeature) {
                 serializer = KotlinxSerializer()
+            }
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Napier.v(tag = "HTTP Client", message = message)
+                    }
+                }
+                level = LogLevel.INFO
             }
         }
     }
