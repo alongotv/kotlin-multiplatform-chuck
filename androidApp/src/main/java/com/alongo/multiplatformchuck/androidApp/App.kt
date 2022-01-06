8562a0c5778f1ed9e983593aeb2811de071fe0c9
vlavet <pdastore1@gmail.com>
2022-01-07 02:03:34 +0300

    Make App DIAware

--- androidApp/src/main/java/com/alongo/multiplatformchuck/androidApp/App.kt
+++ androidApp/src/main/java/com/alongo/multiplatformchuck/androidApp/App.kt
@@ -2,10 +2,29 @@ package com.alongo.multiplatformchuck.androidApp
 
 import android.app.Application
 import com.alongo.multiplatformchuck.shared.common.logging.LoggerInitializer
+import com.alongo.multiplatformchuck.shared.di.androidModule
+import com.alongo.multiplatformchuck.shared.di.data.dataModule
+import com.alongo.multiplatformchuck.shared.di.domain.domainModule
+import com.alongo.multiplatformchuck.shared.di.presentation.presentationModule
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.android.x.androidXModule
 
-class App : Application() {
+class App : Application(), DIAware {
     override fun onCreate() {
         super.onCreate()
         LoggerInitializer.initialize()
+    }
+
+    override val di by lazy {
+        DI.lazy {
+            //FIXME: extension of kodeinContainer does not import dependencies
+            //extend(Injector.kodeinContainer)
+            import(androidXModule(this@App))
+            import(androidModule)
+            importAll(
+                dataModule, domainModule, presentationModule
+            )
+        }
     }
 }
