5df7a228a5f93b3365ba39eca45bfb99d51addd5
vlavet <pdastore1@gmail.com>
2022-01-07 02:12:34 +0300

    Add domainModule, presentationModule, dispatcherProviderModule

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/di/Injector.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/di/Injector.kt
@@ -1,8 +1,12 @@
 package com.alongo.multiplatformchuck.shared.di
 
+import com.alongo.multiplatformchuck.shared.di.common.dispatchers.dispatcherProviderModule
 import com.alongo.multiplatformchuck.shared.di.data.dataModule
+import com.alongo.multiplatformchuck.shared.di.domain.domainModule
+import com.alongo.multiplatformchuck.shared.di.presentation.presentationModule
 import org.kodein.di.DI
 
 object Injector {
-    val kodeinContainer = DI.lazy { importAll(dataModule) }
+    val kodeinContainer =
+        DI.lazy { importAll(dataModule, domainModule, presentationModule, dispatcherProviderModule) }
 }
