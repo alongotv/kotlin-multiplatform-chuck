3e8dd9a07cc9a4603af2025cb38ebab502575018
vlavet <pdastore1@gmail.com>
2022-01-07 02:07:12 +0300

    Add BaseViewModel

---
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/presentation/base/BaseViewModel.kt
@@ -0,0 +1,22 @@
+package com.alongo.multiplatformchuck.shared.presentation.base
+
+import com.alongo.multiplatformchuck.shared.common.dispatchers.DispatcherProvider
+import com.alongo.multiplatformchuck.shared.di.Injector
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import org.kodein.di.instance
+
+abstract class BaseViewModel(val dispatchers: DispatcherProvider) {
+    private val _errors = MutableSharedFlow<Throwable>()
+    val errors = _errors.asSharedFlow()
+
+    protected val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        _errors.tryEmit(throwable)
+    }
+
+    protected val viewModelScope =
+        CoroutineScope(SupervisorJob() + dispatchers.immediate() + coroutineExceptionHandler)
+}
