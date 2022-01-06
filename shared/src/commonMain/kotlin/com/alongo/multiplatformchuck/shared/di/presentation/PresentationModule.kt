c068c4a92e63876e9654ca9a941385cf7c878ad7
vlavet <pdastore1@gmail.com>
2022-01-07 02:12:16 +0300

    Add PresentationModule.kt

---
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/di/presentation/PresentationModule.kt
@@ -0,0 +1,8 @@
+package com.alongo.multiplatformchuck.shared.di.presentation
+
+import com.alongo.multiplatformchuck.shared.di.presentation.main.mainViewModelModule
+import org.kodein.di.DI
+
+val presentationModule = DI.Module("PresentationModule") {
+    import(mainViewModelModule)
+}
