01ffa3c31c40f55de2222c3cfea30f562a351db4
vlavet <pdastore1@gmail.com>
2022-01-07 02:14:26 +0300

    Use withContext on network calls

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/datasource/network/base/BaseApi.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/data/datasource/network/base/BaseApi.kt
@@ -1,26 +1,33 @@
 package com.alongo.multiplatformchuck.shared.data.datasource.network.base
 
+import com.alongo.multiplatformchuck.shared.common.dispatchers.DispatcherProvider
 import com.alongo.multiplatformchuck.shared.di.Injector
 import io.ktor.client.HttpClient
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.http.Url
+import kotlinx.coroutines.withContext
 import org.kodein.di.instance
 
 abstract class BaseApi {
 
     abstract val baseUrl: String
     val httpClient: HttpClient by Injector.kodeinContainer.instance()
+    val dispatcherProvider: DispatcherProvider by Injector.kodeinContainer.instance()
 
     suspend inline fun <reified T : Any> get(
         endpoint: String,
-        block: HttpRequestBuilder.() -> Unit = {}
+        crossinline block: HttpRequestBuilder.() -> Unit = {}
     ): T =
-        httpClient.get(url = Url(baseUrl + endpoint), block = block)
+        withContext(dispatcherProvider.io()) {
+            httpClient.get(url = Url(baseUrl + endpoint), block = block)
+        }
 
     suspend inline fun <reified T : Any> post(
         endpoint: String,
-        block: HttpRequestBuilder.() -> Unit = {}
-    ): T = httpClient.post(url = Url(baseUrl + endpoint), block = block)
+        crossinline block: HttpRequestBuilder.() -> Unit = {}
+    ): T = withContext(dispatcherProvider.io()) {
+        httpClient.post(url = Url(baseUrl + endpoint), block = block)
+    }
 }
