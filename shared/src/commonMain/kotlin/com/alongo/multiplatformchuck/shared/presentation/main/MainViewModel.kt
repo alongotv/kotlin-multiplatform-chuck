7e95254803241948a7869cad34b2d0cfd346fce2
vlavet <pdastore1@gmail.com>
2022-01-07 04:36:36 +0300

    Add isLoading state

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/presentation/main/MainViewModel.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/presentation/main/MainViewModel.kt
@@ -5,7 +5,9 @@ import com.alongo.multiplatformchuck.shared.domain.entity.dto.jokes.JokeDto
 import com.alongo.multiplatformchuck.shared.domain.usecase.joke.get.GetRandomJokeUseCase
 import com.alongo.multiplatformchuck.shared.presentation.base.BaseViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel(
@@ -16,9 +18,18 @@ class MainViewModel(
     private val _jokes = MutableSharedFlow<JokeDto>()
     val jokes = _jokes.asSharedFlow()
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    fun didTapGetRandomJokeButton() {
+        getRandomJoke()
+    }
+
     fun getRandomJoke() {
         viewModelScope.launch {
+            _isLoading.emit(true)
             val joke = getRandomJokeUseCase()
+            _isLoading.emit(false)
             offerJoke(joke)
         }
     }
