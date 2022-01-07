d6c610f73d7bd630484c5329053004a85ba72372
vlavet <pdastore1@gmail.com>
2022-01-07 04:15:22 +0300

    Use emit instead of tryEmit

--- shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/presentation/base/BaseViewModel.kt
+++ shared/src/commonMain/kotlin/com/alongo/multiplatformchuck/shared/presentation/base/BaseViewModel.kt
@@ -3,16 +3,20 @@ package com.alongo.multiplatformchuck.shared.presentation.base
 import com.alongo.multiplatformchuck.shared.common.dispatchers.DispatcherProvider
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel(val dispatchers: DispatcherProvider) {
     private val _errors = MutableSharedFlow<Throwable>()
     val errors = _errors.asSharedFlow()
 
     protected val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        _errors.tryEmit(throwable)
+        CoroutineScope(Dispatchers.Main).launch {
+            _errors.emit(throwable)
+        }
     }
 
     protected val viewModelScope =
