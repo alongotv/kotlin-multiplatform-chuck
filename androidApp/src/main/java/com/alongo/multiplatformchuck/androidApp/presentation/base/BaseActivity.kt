3aafac082df4712347f4b6fea284697d6bd31e42
vlavet <pdastore1@gmail.com>
2022-01-07 03:57:59 +0300

    Cancel coroutine children on Activity onPause

--- androidApp/src/main/java/com/alongo/multiplatformchuck/androidApp/presentation/base/BaseActivity.kt
+++ androidApp/src/main/java/com/alongo/multiplatformchuck/androidApp/presentation/base/BaseActivity.kt
@@ -5,16 +5,23 @@ import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
 open class BaseActivity : AppCompatActivity(), DIAware {
     override lateinit var di: DI
-    protected val activityScope = CoroutineScope(Dispatchers.Main.immediate)
+    protected val activityScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         di = (applicationContext as DIAware).di
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityScope.coroutineContext.cancelChildren()
     }
 }
